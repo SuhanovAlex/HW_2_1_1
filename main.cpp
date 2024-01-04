4d1afda984e2f72156ce5b8497732ea1256e771f
Alexandr Suhanov <delosk@gmail.com>
2024-01-04 16:36:02 +0300

    changes after checking

--- main.cpp
+++ main.cpp
@@ -33,19 +33,19 @@ int main() {
     }
     else{
         months month = static_cast<months>(monthInput);
-        switch (static_cast<int>(month)){
-          case 1: std::cout << "Январь" << std::endl; break;
-          case 2: std::cout << "Февраль" << std::endl; break;
-          case 3: std::cout << "Март" << std::endl; break;
-          case 4: std::cout << "Апрель" << std::endl; break;
-          case 5: std::cout << "Май" << std::endl; break;
-          case 6: std::cout << "Июнь" << std::endl; break;
-          case 7: std::cout << "Июль" << std::endl; break;
-          case 8: std::cout << "Август" << std::endl; break;
-          case 9: std::cout << "Сентябрь" << std::endl; break;
-          case 10: std::cout << "Октябрь" << std::endl; break;
-          case 11: std::cout << "Ноябрь" << std::endl; break;
-          case 12: std::cout << "Декабрь" << std::endl; break;
+        switch (month){
+          case months::Jan: std::cout << "Январь" << std::endl; break;
+          case months::Feb: std::cout << "Февраль" << std::endl; break;
+          case months::Mar: std::cout << "Март" << std::endl; break;
+          case months::Apr: std::cout << "Апрель" << std::endl; break;
+          case months::May: std::cout << "Май" << std::endl; break;
+          case months::Jun: std::cout << "Июнь" << std::endl; break;
+          case months::Jul: std::cout << "Июль" << std::endl; break;
+          case months::Aug: std::cout << "Август" << std::endl; break;
+          case months::Sep: std::cout << "Сентябрь" << std::endl; break;
+          case months::Oct: std::cout << "Октябрь" << std::endl; break;
+          case months::Nov: std::cout << "Ноябрь" << std::endl; break;
+          case months::Dec: std::cout << "Декабрь" << std::endl; break;
           default: std::cout << "Что-то пошло не так!" << std::endl; 
       }
     }
